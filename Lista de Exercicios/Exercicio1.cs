1abfd09d6741003ecd9532763cd32ee3db59de02
Edmilson Nascimento <edmiilsonnascimeento@gmail.com>
2024-03-17 18:15:30 -0300

    update program.cs

--- Lista de Exercicios/Exercicio1.cs
+++ Lista de Exercicios/Exercicio1.cs
@@ -8,7 +8,7 @@ namespace Lista_de_Exercicios
 {
     internal class Exercicio1
     {
-        static void main(string[] args)
+        public static void main(string[] args)
         {
             //Passo 1: Receber a Idade do Usuario.
             Console.WriteLine("Digite sua Idade:");
