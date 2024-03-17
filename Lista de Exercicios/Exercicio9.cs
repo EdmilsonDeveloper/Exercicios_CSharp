016321cebda2fddb98fc58363f950cf865c8af6f
Edmilson Nascimento <edmiilsonnascimeento@gmail.com>
2024-03-17 15:25:14 -0300

    alteração nos comentarios

--- Lista de Exercicios/Exercicio9.cs
+++ Lista de Exercicios/Exercicio9.cs
@@ -10,18 +10,23 @@ namespace Lista_de_Exercicios
     {
         static void Main(string[] args)
         {
-            int altura = 10; //Passo 1: Definir a altura da árvore.
+            //Passo 1: Definir a altura da árvore.
+            int altura = 10; 
 
             for (int i = 0; i < altura; i++)
             {
+                //Passo 2: Definir os Espaços para alinhar os asteriscos.
                 for (int j = 0; j < altura - i - 1; j++)
                 {
-                    Console.Write(" "); //Passo 2: Definir os Espaços para alinhar os asteriscos.
+                    Console.Write(" "); 
                 }
+
+                //Passo 3: Imprimir os asteriscos.
                 for (int k = 0; k < 2 * i + 1; k++)
                 {
-                    Console.Write("*"); //Passo 3: Imprimir os asteriscos.
+                    Console.Write("*"); 
                 }
+
                 Console.WriteLine();
 
                 //Console.ReadKey();
